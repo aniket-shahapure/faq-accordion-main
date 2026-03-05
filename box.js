32514810a937a7c04d9734366bd7c0bfdfa8670b
aniket-shahapure <aniketshahapure17@gmail.com>
2026-03-06 01:13:02 +0530

    USING - HTML CSS JS

---
+++ box.js
@@ -0,0 +1,31 @@
+function select(para, minus, plus) {
+  document.getElementById(para).style.display = "flex";
+  document.getElementById(minus).style.display = "flex";
+  document.getElementById(plus).style.display = "none";
+}
+
+// // const { createElement } = require("react");
+
+// function select(para, im, so, type) {
+//   console.log(type, im, so, para);
+//   if (type == "minus") {
+//   } else {
+//   }
+// }
+// createElement()
+// let minus = document.getElementById("minus-3");
+// let plus = document.getElementById("plus-3");
+// console.log(plus);
+// plus.addEventListener("click", () => {
+//   console.log("33333");
+//   document.getElementById("para-3").style.display = "flex";
+//   document.getElementById("im-3").style.display = "flex";
+//   document.getElementById("so-3").style.display = "none";
+// });
+
+// minus.addEventListener("click", () => {
+//   console.log("000000");
+//   document.getElementById(para).style.display = "none";
+//   document.getElementById(im).style.display = "none";
+//   document.getElementById(so).style.display = "flex";
+// });
